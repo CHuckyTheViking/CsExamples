f03bdb699335afd19df570df246a153f49cbb919
Jesper Müllern <mullern23@hotmail.com>
2020-09-01 14:16:34 +0200

    C7

---
+++ C7_GettersAndSettersV2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C7_GettersAndSettersV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            //  person.AskForName();
+            // person.AskForAge();
+            // person.NameContains();
+            //person.AskForCity();
+            person.AskForDateOfBirth();
+
+            Console.ReadKey();
+        }
+    }
+}
