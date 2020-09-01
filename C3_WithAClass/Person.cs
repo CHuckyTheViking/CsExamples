67a946e5f411b7ff9e18bafec38fb42a214d9ea0
Jesper Müllern <mullern23@hotmail.com>
2020-09-01 11:33:07 +0200

    C3

---
+++ C3_WithAClass/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C3_WithAClass
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+
+    }
+
+    
+}
---
+++ C3_WithAClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C3_WithAClass
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            Console.Write("Welcome dude! What's your name? ");
+            person.Name = Console.ReadLine();
+
+            Console.Write($"{person.Name} sweet :) How old are you man? ");
+
+            try
+            {
+                person.Age = Convert.ToInt32(Console.ReadLine());
+                //age= int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Sorry dude! your age must be in digits. Try again man.");
+                Console.Write("How old are you man? ");
+                person.Age = Convert.ToInt32(Console.ReadLine());
+
+            }
+            finally
+            {
+                Console.Write($"Cool man! We are the same age, i'm also {person.Age}.");
+            }
+
+            Console.WriteLine($"\nDude your name contains {person.Name.Length} characters.");
+            Console.Write($"{person.Name} where do you live? ");
+            person.City = Console.ReadLine();
+
+            Console.WriteLine($"Sweet! My brother also lives in {person.City}");
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
