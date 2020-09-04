431ea766f250235ae8e329bcfbdc2f20a22c91b6
Jesper Müllern <mullern23@hotmail.com>
2020-09-04 10:20:42 +0200

    C12

---
+++ C12_Interfaces_1/BandSaw.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C12_Interfaces_1
+{
+    class BandSaw : IMachine
+    {
+        public bool Start()
+        {
+            Console.WriteLine("Bandsaw has started.");
+            return true;
+        }
+
+        public bool Stop()
+        {
+            Console.WriteLine("Bandsaw has stopped.");
+            return true;
+        }
+    }
+}
