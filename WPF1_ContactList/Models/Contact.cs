6cdb3c6322facd0732c9d32fdc1a0d8aa7a71f37
Jesper Müllern <mullern23@hotmail.com>
2020-09-08 15:53:47 +0200

    WINFORM&WPF

---
+++ WPF1_ContactList/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace WPF1_ContactList.Models
+{
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Fullname => $"{FirstName} {LastName}";
+
+        public Contact(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+    }
+}
